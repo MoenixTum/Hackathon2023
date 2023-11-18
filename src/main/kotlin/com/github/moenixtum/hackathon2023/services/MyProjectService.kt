6acbe1678e5d0e34c556b270777411e22a02a9c8
GitHub Action <action@github.com>
2023-11-18 12:17:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/moenixtum/hackathon2023/services/MyProjectService.kt
+++ src/main/kotlin/com/github/moenixtum/hackathon2023/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.moenixtum.hackathon2023.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.moenixtum.hackathon2023.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
